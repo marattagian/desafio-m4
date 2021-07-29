99825931ca70ae5290a7a55e018096f3c1113c8e
Gian <marattagian15@gmail.com>
2021-07-28 21:15:17 -0300

    index.html already connected with contentful

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function main() {
+async function main() {
   const headerParent = document.querySelector(".header-container")
   addHeader(headerParent)
   const footerParent = document.querySelector(".footer-container")
@@ -8,19 +8,19 @@ function main() {
 
   addSubmitListener(document.querySelector(".contact__form"))
 
-  // const spaceId = ""
-  // const envId = ""
-  // const previewToken = ""
-  // let url = `https://preview.contentful.com/spaces/${spaceId}/environments/${envId}/entries?access_token=${previewToken}`
+  const spaceId = "twfbnrw5coxm"
+  const envId = "master"
+  const previewToken = "t1TMliybPN0nX5-IKkLdDLKkGlJeDClwLrLsW8zvRnM"
+  let url = `https://preview.contentful.com/spaces/${spaceId}/environments/${envId}/entries?access_token=${previewToken}`
 
-  // const request = await fetch(url)
-  // const data = await request.json()
-  // showCards(data)
+  const request = await fetch(url)
+  const data = await request.json()
+  showCards(data)
 
 }
 
 function fillCardData(params) {
-  const template = document.querySelector("#card-to-clone")
+  const template = document.querySelector("#container-to-copy")
   const clone = template.content.cloneNode(true)
 
   clone.querySelector(".card__info-title").textContent = params.title
@@ -32,22 +32,23 @@ function fillCardData(params) {
 }
 
 function showCards(jsonData) {
-  console.log(jsonData)
   const data = jsonData.items
   const assets = jsonData.includes.Asset
 
   data.map(item => {
-    const imageID = item.fields.image.sys.id
+    const imageID = item.fields.logo.sys.id || ""
     const result = assets.filter(asset => asset.sys.id == imageID)
 
-    const cardObject = {
-      title: item.fields.title,
-      description: item.fields.description,
-      url: item.fields.url,
-      imageUrl: result[0].fields.file.url,
+    if (imageID) {
+      const cardObject = {
+        title: item.fields.title,
+        description: item.fields.description,
+        imageUrl: result[0].fields.file.url,
+      }
+
+      return fillCardData(cardObject)
     }
 
-    return fillCardData(cardObject)
   })
 }
 
